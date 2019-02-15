1a2bf170dfc262e088e294db2aa3682c2dabd9a7
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-15 17:28:32 -0500

    Completed phonebook lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,96 @@
 package com.zipcodewilmington.phonebook;
 
+
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private String number;
+    private String homeAddress;
+    private String email;
+    private String name;
+    private ArrayList<String> contact;
+    public TreeMap<String, ArrayList> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new TreeMap<String, ArrayList>();
+    }
+
+    public ArrayList<String> createContact(String number, String homeAddress, String email) {
+        this.number = number;
+        this.homeAddress = homeAddress;
+        this.email = email;
+
+        ArrayList<String> contacts = new ArrayList<String>();
+        contacts.add(number);
+        contacts.add(homeAddress);
+        contacts.add(email);
+
+        return contacts;
+
+    }
+
+    public void add(String name, ArrayList<String> contact) {
+
+        this.name = name;
+        this.contact = contact;
+        phoneBook.put(name, contact);
+
+    }
+
+    public void remove(String name) {
+
+        this.name = name;
+        phoneBook.remove(name);
+
+    }
+
+    public ArrayList<String> lookup(String name) {
+        this.name = name;
+
+        return phoneBook.get(name);
+
+    }
+
+    public Object reverseLookUp(ArrayList<String> contact) {
+
+        this.contact = contact;
+        for (Map.Entry s : phoneBook.entrySet()) {
+            if (s.getValue() == contact) {
+                return s.getKey();
+            }
+
+        }
+        return null;
+
+    }
+
+    public void display(){
+        //this.phoneBook = phoneBook;
+        Set<Map.Entry<String, ArrayList>> set = phoneBook.entrySet();
+
+        for(Map.Entry<String, ArrayList> s : set){
+            System.out.print(s.getKey() + "'s contact information is: ");
+            System.out.println(s.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Sean", phoneBook.createContact("484-574-3999", "1026 Edgerton Rd. Secane, PA 19018", "nope@nope.com"));
+        phoneBook.add("Cara", phoneBook.createContact("123-321-2233", "somewhere, USA", "yep@nope.com"));
+        phoneBook.add("Mike", phoneBook.createContact("321-123-3322", "somwhere,USA", "howboutNope@nope.com"));
+
+        phoneBook.display();
+    }
 }
+
+
+
+
+
+
+
+
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,85 @@
 package com.zipcodewilmington.phonebook;
 
+//import jdk.internal.jline.internal.TestAccessible;
+import org.junit.Assert;
+//import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+
+    @Test
+    public void addTest() {
+        //Given
+        //Created new instance of PhoneBook
+        PhoneBook testBook = new PhoneBook();
+        //Created contact using createContact Method
+        ArrayList<String> contact = testBook.createContact("484-574-3999", "1026 Edgerton Rd", "seanwrowan@gmail.com");
+        //Expected size of TreeMap should be current size + 1 more after adding 1 key/value
+        int expectedSize = testBook.phoneBook.size() + 1;
+        //When
+        testBook.add("Sean", contact);
+        int actualSize = testBook.phoneBook.size();
+        //Then
+        Assert.assertEquals(expectedSize, actualSize);
+
+    }
+
+    @Test
+    public void removeTest() {
+        //Given
+        PhoneBook testBook = new PhoneBook();
+        ArrayList<String> contact = testBook.createContact(null, null, null);
+        testBook.add("sean", contact);
+        int expected = testBook.phoneBook.size() - 1;
+        //When
+        testBook.remove("sean");
+        int actual = testBook.phoneBook.size();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lookupTest() {
+        //Given
+        PhoneBook testBook = new PhoneBook();
+        ArrayList<String> contact = testBook.createContact(null, null, null);
+        testBook.add("sean", contact);
+        //When
+        ArrayList<String> actual = testBook.lookup("sean");
+        //Then
+        Assert.assertEquals(contact, actual);
+
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        //Given
+        PhoneBook testBook = new PhoneBook();
+        ArrayList<String> contact = testBook.createContact("555-555-5555", "here", "nope");
+        testBook.add("sean", contact);
+        //When
+        Object actual = testBook.reverseLookUp(contact);
+        //Then
+        Assert.assertEquals("sean", actual);
+    }
+
+//    @Test
+//    public void displayTest(){
+//        //Given
+//        PhoneBook testBook = new PhoneBook();
+//        ArrayList<String> contact = new ArrayList<String>();
+//        contact.add("484-574-3999");
+//        testBook.add("sean", contact);
+//        //Then
+//        Assert.assertEquals("sean : 484-574-3999", testBook.display());
+//    }
+
+
 }
